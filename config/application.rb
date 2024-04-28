5979879c0e02e55949f9aba815e75a0ffc64e7c0
s17w09 <s0917.smys04@gmail.com>
2024-04-28 21:03:09 +0900

    GitHub連携

---
+++ config/application.rb
@@ -0,0 +1,32 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module KodomoGokoro
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.1
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.helper false             # helper ファイルを作成しない
+      g.test_framework false     # test ファイルを作成しない
+      g.skip_routes true         # ルーティングの記述を作成しない
+    end
+  end
+end
