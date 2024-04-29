67c3f9cf06204e4c642af9df1fa038011689eeeb
s17w09 <s0917.smys04@gmail.com>
2024-04-29 09:07:09 +0900

    画面遷移レイアウトまで作成

---
+++ app/controllers/dreams_controller.rb
@@ -0,0 +1,14 @@
+class DreamsController < ApplicationController
+  def index
+
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
---
+++ app/controllers/openais_controller.rb
@@ -0,0 +1,7 @@
+class OpenaisController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+end
