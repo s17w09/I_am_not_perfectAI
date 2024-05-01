275317f8f63beeff5f6c1f3925f30b4591a6b470
s17w09 <s0917.smys04@gmail.com>
2024-05-01 09:03:02 +0900

    Dall3導入完了

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,4 +1,22 @@
 class DreamsController < ApplicationController
-  def new
-  end
+    def index
+      @image_url = session[:image_url]
+      @error = session[:error]
+    end
+  
+    def create
+      prompt = params[:prompt]
+      response = OpenAiService.generate_image(prompt)
+
+      Rails.logger.info "OpenAI API Response: #{response}"
+    
+      if response.key?("error")
+        session[:error] = response["error"]
+      else
+        session[:image_url] = response["data"][0]["url"]
+        session[:error] = nil
+      end
+    
+      redirect_to dreams_path
+    end
 end
---
+++ app/services/open_ai_service.rb
@@ -0,0 +1,27 @@
+class OpenAiService
+  include HTTParty
+
+  base_uri 'https://api.openai.com/v1'
+
+  def self.generate_image(prompt, size="1024x1024", quality="standard", n=1)
+    begin
+      response = post('/images/generations',
+                      headers: { 
+                        'Authorization' => "Bearer #{ENV['OPENAI_API_KEY']}",
+                        'Content-Type' => 'application/json'
+                      },
+                      body: { 
+                        model: 'dall-e-3', 
+                        prompt: prompt, 
+                        size: size, 
+                        quality: quality, 
+                        n: n 
+                      }.to_json)
+      raise "Error: #{response.code} - #{response.message}" unless response.success?
+
+      response.parsed_response
+    rescue => e
+      { error: e.message }
+    end
+  end
+end
