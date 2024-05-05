670087f413020e00093a676fcf17e60688a57ed8
s17w09 <s0917.smys04@gmail.com>
2024-05-05 17:12:05 +0900

    content forでタイトル修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def page_title(title = "")
+    base_ttitle = "I'm a NOT PERFECT AI"
+    if title.empty?
+      base_ttitle
+    else
+       "#{title} | #{base_ttitle}"
+    end
+  end
 end
